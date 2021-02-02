3ce15ed8cf4fe01d26428e19c3c53f7e860a5d41
Jonah Vanke <jdv24@cam.ac.uk>
2021-02-02 23:56:07 +0000

    fix to test_stations_by_distance

--- test_stations_by_distance.py
+++ test_stations_by_distance.py
@@ -1,15 +1,16 @@
 from testingData import getFakeData
 from floodsystem.geo import stations_by_distance
-stations = getFakeData()
+
 
 '''
 Tests stations_by_distance on given testingData data (not subject to change).  If this has broken later on,
 you likely did something that changed the coordinates of testingData.  Either go back and fix it
 or find the new values to put in here.
 '''
-
-assert stations_by_distance(stations, (52.2053, 0.1218)) == [(stations[3], round(0.5344202805927869, 8)),
-                                                             (stations[2], round(0.8362136220642843, 8)),
-                                                             (stations[4], round(4.118936016701786, 8)),
-                                                             (stations[0], round(6201.8298217394495, 8)),
-                                                             (stations[1], round(12440.59397766243, 8))]
+def test_stations_by_distance():
+    stations = getFakeData()
+    assert stations_by_distance(stations, (52.2053, 0.1218)) == [(stations[3], round(0.5344202805927869, 8)),
+                                                                (stations[2], round(0.8362136220642843, 8)),
+                                                                (stations[4], round(4.118936016701786, 8)),
+                                                                (stations[0], round(6201.8298217394495, 8)),
+                                                                (stations[1], round(12440.59397766243, 8))]
