a8316bc5fa03637eb5179ffae535fea02b1f139b
Jonah Vanke <jdv24@cam.ac.uk>
2021-02-02 23:50:19 +0000

    grammar for testingData attempt 1

--- testingData.py
+++ testingData.py
@@ -30,24 +30,25 @@ class MonitoringStation:
         d += "   typical range: {}".format(self.typical_range)
         return d
 
+
 def getFakeData():
     stations = []
 
     RoanokeRiver = MonitoringStation(station_id="station_id", measure_id="measure_id", label="Smith Park",
-                                    coord=(37.2574, -79.9544), typical_range="typical_range", 
-                                    river="Roanoke River", town="Roanoke")
+                                     coord=(37.2574, -79.9544), typical_range="typical_range",
+                                     river="Roanoke River", town="Roanoke")
     NahuelHuapi =  MonitoringStation(station_id="station_id", measure_id="measure_id", label="Mitre",
-                                    coord=(-41.1314, -71.2933), typical_range="typical_range", 
-                                    river="Lago Nahuel Huapi", town="San Carlos de Bariloche")
+                                     coord=(-41.1314, -71.2933), typical_range="typical_range",
+                                     river="Lago Nahuel Huapi", town="San Carlos de Bariloche")
     JesusLock = MonitoringStation(station_id="station_id", measure_id="measure_id", label="Jesus Lock",
-                                    coord=(52.2128, 0.1209), typical_range="typical_range", 
-                                    river="River Cam", town="Cambridge")
+                                  coord=(52.2128, 0.1209), typical_range="typical_range",
+                                  river="River Cam", town="Cambridge")
     OrgasmBridge = MonitoringStation(station_id="station_id", measure_id="measure_id", label="Garret Hostel Bridge",
-                                    coord=(52.2058, 0.1140), typical_range="typical_range", 
-                                    river="River Cam", town="Cambridge")
+                                     coord=(52.2058, 0.1140), typical_range="typical_range",
+                                     river="River Cam", town="Cambridge")
     ByronsPool = MonitoringStation(station_id="station_id", measure_id="measure_id", label="Byron's Pool",
-                                    coord=(52.1712,0.0982), typical_range="typical_range", 
-                                    river="River Cam", town="Cambridge")
+                                   coord=(52.1712,0.0982), typical_range="typical_range",
+                                   river="River Cam", town="Cambridge")
 
     stations.append(RoanokeRiver)
     stations.append(NahuelHuapi)
@@ -56,12 +57,14 @@ def getFakeData():
     stations.append(ByronsPool)
     return stations
 
-#print(stations)
+
 '''
+print(stations)
+
 def getFakeData():
     print(stations)
-    return stations'''
-#print(stations)
-#print(getFakeData())
-
-#print(type(stations))
+    return stations
+print(stations)
+print(getFakeData())
+print(type(stations))
+'''
