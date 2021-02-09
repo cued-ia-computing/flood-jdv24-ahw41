7d40a0a18e3613cde9a3e980ae12c997147b505c
Jonah Vanke <jdv24@cam.ac.uk>
2021-02-09 22:02:47 +0000

    Task 1D update

--- Task1D.py
+++ Task1D.py
@@ -5,15 +5,9 @@ from floodsystem.geo import stations_by_river
 stations = build_station_list()
 riversWithStation = rivers_with_station(stations)
 print(len(riversWithStation), "stations. First 10 -", sorted(riversWithStation)[:10])
-aire = []
-cam = []
-thames = []
-for station in stations_by_river(stations)["River Aire"]:
-    aire.append(station.name)
-for station in stations_by_river(stations)["River Cam"]:
-    cam.append(station.name)
-for station in stations_by_river(stations)["River Thames"]:
-    thames.append(station.name)
-print(sorted(aire))
-print(sorted(cam))
-print(sorted(thames))
+
+for river in ["River Aire", "River Cam", "River Thames"]:
+    arr = []
+    for station in stations_by_river(stations)[river]:
+        arr.append(station.name)
+    print(sorted(arr))
--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -8,7 +8,9 @@ geographical data.
 
 from .utils import sorted_by_key  # noqa
 
-#2/2/2021 implementing distance function
+# 2/2/2021 implementing distance function
+
+
 def haversine(coord1: object, coord2: object):
     # Spherical distance function
     import math
@@ -25,40 +27,35 @@ def haversine(coord1: object, coord2: object):
     delta_lambda = math.radians(lon2 - lon1)
 
     a = math.sin(delta_phi / 2.0) ** 2 + math.cos(phi_1) * math.cos(phi_2) * math.sin(delta_lambda / 2.0) ** 2
-    
+
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
 
     meters = R * c  # output distance in meters
     km = meters / 1000.0  # output distance in kilometers
-    '''
-    meters = round(meters, 3)
-    km = round(km, 3)
-    '''
 
-    #print(f"Distance: {meters} m")
-    #print(f"Distance: {km} km")
     return km
 
 
 def stations_by_distance(stations, p):
-    #returns stations by distancee and their distance as a list
-    distanceList=[]
-    #stations is a list of MonitoringStation objects and p a tuple of floats for the coordinate p
+    # returns stations by distancee and their distance as a list
+    distanceList = []
+    # stations is a list of MonitoringStation objects and p a tuple of floats for the coordinate p
     for station in stations:
-        distance=1
-        distanceList.append((station,round(haversine(p,station.coord),8)))
-    return(sorted_by_key(distanceList,1))
+        distanceList.append((station, round(haversine(p, station.coord), 8)))
+    return(sorted_by_key(distanceList, 1))
+
 
 def stations_within_radius(stations, centre, r):
-    #List of stations within a given radius of a point
+    # List of stations within a given radius of a point
     stationList = []
     for station in stations:
         if haversine(station.coord, centre) < r:
             stationList.append(station)
     return(stationList)
 
+
 def rivers_with_station(stations):
-    #A function that returns rivers with stations
+    # A function that returns rivers with stations
     rivers = set()
     for station in stations:
         rivers.add(station.river)
@@ -66,11 +63,10 @@ def rivers_with_station(stations):
 
 
 def stations_by_river(stations):
-    #Returns river dictionary
+    # Returns river dictionary
     riverDict = {}
     for station in stations:
         if station.river not in riverDict:
             riverDict[station.river] = []
         riverDict[station.river].append(station)
     return(riverDict)
-
